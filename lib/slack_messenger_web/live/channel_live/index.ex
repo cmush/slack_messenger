18f83a40645f1438ceb3cab72955c3c1a0fe6773
cmush <collinsmucheru@gmail.com>
2022-01-05 18:23:45 +0300

    feat: navigate to messages index page with channel id

--- lib/slack_messenger_web/live/channel_live/index.ex
+++ lib/slack_messenger_web/live/channel_live/index.ex
@@ -45,6 +45,13 @@ defmodule SlackMessengerWeb.ChannelLive.Index do
     {:noreply, assign(socket, :channels, list_channels())}
   end
 
+  @impl true
+  def handle_event("open", %{"channel_id" => channel_id}, socket) do
+    {:noreply,
+     socket
+     |> push_redirect(to: Routes.message_index_path(socket, :index, channel_id: channel_id))}
+  end
+
   defp list_channels do
     Channels.list_channels()
   end
